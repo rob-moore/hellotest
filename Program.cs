8b5c5671f55152e625063e23c2550ad8b3da43b9
Rob Moore <robert.moore@allata.com>
2018-09-24 20:21:07 -0600

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,46 @@
-﻿namespace HelloTest
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HelloTest
 {
     internal class Program
-    {
+    {           
         public static void Main(string[] args)
         {
+            // Pick random number
+
+            var random = new Random();
+            var randomNumber = random.Next(1, 10);
+            
+            // Prompt user to guess
+            Console.WriteLine("Pick a number between 1 and 10");
+           
+            // if correct end
+            var running = true;
+            while (running)
+            {
+                var guess = Console.ReadLine();
+                int.TryParse(guess, out int guessInt);
+                
+                if (guessInt == randomNumber)
+                {
+                    Console.WriteLine("Nice job");
+                    running = false;
+                    continue;
+                }
+                // if not tell high or low
+                if (guessInt > randomNumber)
+                {
+                    Console.WriteLine("Too high, try again.");
+                }
+
+                if (guessInt < randomNumber)
+                {
+                    Console.WriteLine("Too low, try again");
+                }
+            }
+            
+
         }
     }
 }
